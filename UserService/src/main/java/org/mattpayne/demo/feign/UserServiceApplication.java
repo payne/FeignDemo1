8162ee4c2a9ea39ae894d668aa5d4bf9210835f4
Matt Payne <Payne@MattPayne.org>
2025-07-02 21:49:27 -0500

    create a user when fetching them.  Lame and simple.

--- UserService/src/main/java/org/mattpayne/demo/feign/UserServiceApplication.java
+++ UserService/src/main/java/org/mattpayne/demo/feign/UserServiceApplication.java
@@ -8,15 +8,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 @RestController
 public class UserServiceApplication {
     private final AtomicInteger requestCount = new AtomicInteger(0);
+    private final Set<User> users = new HashSet<>();
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<Set<User>> getUsers() {
+        return ResponseEntity.ok(users);
     }
 
     @GetMapping("/users/{id}")
@@ -27,6 +35,7 @@ public class UserServiceApplication {
             throw new RuntimeException("Service temporarily unavailable");
         }
         User user = new User(id, "John Doe", "john@example.com");
+        users.add(user); // ugly but quick way to add user for demo purposes
         return ResponseEntity.ok(user);
     }
 }
